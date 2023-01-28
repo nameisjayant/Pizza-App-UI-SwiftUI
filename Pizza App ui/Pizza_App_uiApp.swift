c4d7b8753baa0060cdf0760d00ab9679cf5a2662
nameisjayant <jayntkumar99@gmail.com>
2023-01-28 12:08:30 +0530

    Initial Commit

---
+++ Pizza App ui/Pizza_App_uiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pizza_App_uiApp.swift
+//  Pizza App ui
+//
+//  Created by jayant kumar on 28/01/23.
+//
+
+import SwiftUI
+
+@main
+struct Pizza_App_uiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
