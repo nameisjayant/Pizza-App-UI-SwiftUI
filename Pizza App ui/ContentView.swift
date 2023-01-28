701c42696d49a2f3eccf0133213cabbe7e7f1fed
nameisjayant <jayntkumar99@gmail.com>
2023-01-28 12:33:07 +0530

    header added

--- Pizza App ui/ContentView.swift
+++ Pizza App ui/ContentView.swift
@@ -9,15 +9,42 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        ZStack(alignment: .leading){
+           
+            VStack{
+                HStack{
+                    HStack{
+                        HStack{
+                            Button{
+                            } label: {
+                                Image(systemName: "list.bullet.indent")
+                                    .resizable()
+                                    .foregroundColor(.white)
+                                    .frame(width: 20,height: 20)
+                            }
+                            Text("Jayant Kumar")
+                                .foregroundColor(.white)
+                                .font(.title3)
+                                .fontWeight(.semibold)
+                                .padding(.leading,15)
+                                
+                        }
+                        Spacer()
+                        Button{
+                        }label: {
+                            Image(systemName: "magnifyingglass")
+                                .resizable()
+                                .foregroundColor(.white)
+                                .frame(width: 20,height: 20)
+                        }
+                        }
+                    }.padding(20)
+                }.background(.red)
+            }
+        .frame(maxWidth: .infinity,maxHeight: .infinity,alignment: .topLeading)
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
