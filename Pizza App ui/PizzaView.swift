b7dfd415f67ab6b5f1b6fa6f09b8d696a4c5c614
nameisjayant <jayntkumar99@gmail.com>
2023-01-29 20:23:03 +0530

    grid view added

--- Pizza App ui/PizzaView.swift
+++ Pizza App ui/PizzaView.swift
@@ -16,7 +16,7 @@ struct ContentView: View {
     var body: some View {
         ZStack(alignment: .leading){
     
-            VStack(){
+            VStack(alignment : .leading){
                 HStack{
                     HeaderView()
                 }.background(.red)
@@ -31,8 +31,55 @@ struct ContentView: View {
                     }.frame(maxWidth: .infinity,alignment: .leading)
                         .padding(15)
                 }
+                Grid() {
+                    GridRow {
+                        ShowPizzView()
+                        ShowPizzView()
+                    }
+                }
             }
             }.frame(maxWidth: .infinity,maxHeight: .infinity,alignment: .topLeading)
+            .background(Color(hex: 0xFFF1F1F1))
+    }
+}
+
+struct ShowPizzView :View{
+    var body: some View{
+        ZStack{
+            Rectangle()
+                .background(.white)
+                .foregroundColor(.white)
+                .cornerRadius(10)
+                .padding()
+            VStack(alignment : .center){
+                Image("pizza")
+                    .resizable()
+                    .frame(width: 109,height: 109)
+                Text("£14.25")
+                    .foregroundColor(.red)
+                    .multilineTextAlignment(.center)
+                Text("Fresh Farm House")
+                    .font(.headline)
+                    .multilineTextAlignment(.center)
+                    .padding(.vertical,1)
+                Text("crisp capsicum, succulent mushrooms and fresh tomatoes")
+                    .multilineTextAlignment(.center)
+                    .font(.caption)
+                    .foregroundColor(.gray)
+                    .fontWeight(.light)
+                Button{
+                    
+                } label: {
+                    Text("Add")
+                        .padding(5)
+                        .frame(width: 90)
+                        .font(.headline)
+                }.buttonStyle(.borderedProminent)
+                    .cornerRadius(25)
+                    .tint(.red)
+
+            }.padding(.horizontal,10)
+        }.frame(width: 230,height: 310)
     }
 }
 
@@ -58,13 +105,6 @@ struct CustomChipView : View {
     }
 }
 
-
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
 
 struct HeaderView : View{
     var body: some View{
@@ -94,5 +134,23 @@ struct HeaderView : View{
                     
                 }
                 }.padding(20)
+    }
+}
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+
+extension Color {
+    init(hex: Int, opacity: Double = 1.0) {
+        let red = Double((hex & 0xff0000) >> 16) / 255.0
+        let green = Double((hex & 0xff00) >> 8) / 255.0
+        let blue = Double((hex & 0xff) >> 0) / 255.0
+        self.init(.sRGB, red: red, green: green, blue: blue, opacity: opacity)
     }
 }
